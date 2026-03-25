aac85958fde2a192dab04b4316452a80d9a2f840
felix <github-fe@pfoertner-net.de>
2026-03-25 17:55:17 +0100

    update random collections

--- data/random_collections/count_reads_per_sample.sh
+++ data/random_collections/count_reads_per_sample.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=count_fqs
 
-output="read_counts.tsv"
+output="read_counts_2.tsv"
 echo -e "File\tReads" > "$output"
 
-for file in /data/share/htp/prime-seq_NextGen/data/data_collection_for_submission/demultiplexed_fastqs/*.fq.gz; do
+for file in /data/share/htp/prime-seq_NextGen/data/data_collection_for_submission/second_submission/demultiplexed_fastqs/*.fq.gz; do
     [[ -f "$file" ]] || continue
 
     lines=$(gzip -cd "$file" | wc -l)
